b42089a23037d767429978373de623001d66826d
B-Tsai <benjamin.cm.tsai@gmail.com>
2018-01-17 15:55:18 -0500

    case_autorun has been tested, work fine

--- case_autorun.sh
+++ case_autorun.sh
@@ -11,6 +11,7 @@ source ./settings.sh
 # Submit the job for the first time
 echo "First time submitting the job ..."
 job_submit $$
+echo "Wait 30 sec to make sure the job has been submitted ..."
 sleep 30
 
 # Start checking job status for 
@@ -23,12 +24,13 @@ do
     if [[ $job_stat == 0 ]] ; then
         echo "Job does not in queue, re-sumbiting ..."
         job_submit $$
+        echo "Wait 30 sec to make sure the job has been submitted ..."
         sleep 30
-        retry_times=$retry_times+1
+        retry_times=$(($retry_times+1))
     else
-        echo "Job is in queue, waiting 300 sec ..."
-        sleep 300
+        echo "Job is in queue, waiting 30 sec for the next check..."
+        sleep 30
         retry_times=0
     fi
 done
-echo "Retry more than 5 times, process stop!"
+echo "Retry more than 5 times, process stops!"
--- modules/job.sh
+++ modules/job.sh
@@ -7,7 +7,7 @@
 function job_submit()
 {
     # Submit the job and get its job-ID
-    job_sub_msg=`ssh $user_name@$remote_host "workgroup -g $group_name -C 'qsub $case_dir/$qs_file'"` $$
+    job_sub_msg=`ssh $user_name@$remote_host "workgroup -g $group_name -C 'qsub $case_dir/$qs_file'"` 
     job_ID=${job_sub_msg//[^0-9]/}
 }
 
@@ -15,7 +15,7 @@ function job_submit()
 function job_check()
 {
     # Get job stat
-    job_stat_msg=`ssh $user_name@$remote_host "qstat -u $user_name -j $job_ID"` $$
+    job_stat_msg=`ssh $user_name@$remote_host "qstat -u $user_name -j $job_ID"` 
     if [[ $job_stat_msg == "" ]] ; then
         job_stat=0
     else
