b5f9c6da64301cd3a0e89907f2aa3fdd404d9479
B-Tsai <benjamin.cm.tsai@gmail.com>
2018-01-17 19:04:48 -0500

    Allowed data_remove() to keep 2 latest time steps data

--- modules/data.sh
+++ modules/data.sh
@@ -15,14 +15,15 @@ function data_remove()
 {
     # Get the time step list on local host
     list_time_step=`ssh $user_name@$remote_host "foamListTimes -case $case_dir_local"`
+    read -a array_time_step <<<$list_time_step
     
     # Get the latest time step on local host
     latest_time_step=`ssh $user_name@$remote_host "foamListTimes -case $case_dir_local -latestTime"`
 
-    # Remove data on remote host
-    for time_step in $list_time_step
+    # Remove data on remote host (still keep 2 latest time steps)
+    for time_step in ${array_time_step[@]}
     do
-        if [[ $time_step != $latest_time_step ]] ; then
+        if [ $time_step != ${array_time_step[-1]} ] && [ $time_step != ${array_time_step[-2]} ] ; then
             `ssh $user_name@$remote_host "rm -rf $case_dir_remote/processor*/$time_step"`
         fi
     done
